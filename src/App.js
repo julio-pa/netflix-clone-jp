323c10f7c690d4ff2530408d286ccf5272d838a5
Julio Patiño <franjp132@gmail.com>
2022-12-02 13:50:35 -0500

    Creando rutas y estilos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { makeStyles } from '@material-ui/core';
+import Login from './Pages/Login';
+import Profile from './Pages/Profile';
+import Paypal from './Pages/Paypal';
+import Home from './Pages/Home';
+
+
 
 
 function App() {
+  const classes = useStyles();
+
+  const user = "luke";
+
   return (
-    <div className="App">
-      <h1>Y ahora estoy aqui de nuevo</h1>
+    <div className={classes.root}>
+      <Router>
+        {
+          !user ? (<Login />) : (
+            <Routes>
+              <Route path='/profile' element={<Profile />}/>
+              <Route path='/checkout' element={<Paypal />}/>
+              <Route path='/' element={<Home />}/>
+            </Routes>
+          )
+        }
+      </Router>
     </div>
   );
-}
+};
+
+const useStyles = makeStyles((theme) => ({
+  root:{
+    // background: "#111",
+    minHeight: "100vh",
+  }
+}))
 
 export default App;
---
+++ src/Pages/Paypal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Paypal = () => {
+  return (
+    <div>
+      Paypal
+    </div>
+  );
+}
+
+export default Paypal;
